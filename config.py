655c1359a978a2cc7181bf45d18a6848f95f7784
Gokuvegeta928 <120127207+Gokuvegeta928@users.noreply.github.com>
2023-01-31 01:25:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ class API:
     API_HASH = os.environ["API_HASH"]
 
 class TOKENS:
-    BOT_TOKEN = os.environ["STRING_SESSION"]
-    BOT_TOKEN_2 = os.environ["STRING_SESSION_2"]
-    BOT_TOKEN_3 = os.environ["STRING_SESSION_3"]
-    BOT_TOKEN_4 = os.environ["STRING_SESSION_4"]
-    BOT_TOKEN_5 = os.environ["STRING_SESSION_5"]
-    BOT_TOKEN_6 = os.environ["STRING_SESSION_6"]
-    BOT_TOKEN_7 = os.environ["STRING_SESSION_7"]
-    BOT_TOKEN_8 = os.environ["STRING_SESSION_8"]
-    BOT_TOKEN_9 = os.environ["STRING_SESSION_9"]
-    BOT_TOKEN_10 = os.environ["STRING_SESSION_10"]
+    STRING_SESSION = os.environ["STRING_SESSION"]
+    STRING_SESSION_2 = os.environ["STRING_SESSION_2"]
+    STRING_SESSION_3 = os.environ["STRING_SESSION_3"]
+    STRING_SESSION_4 = os.environ["STRING_SESSION_4"]
+    STRING_SESSION_5 = os.environ["STRING_SESSION_5"]
+    STRING_SESSION_6 = os.environ["STRING_SESSION_6"]
+    STRING_SESSION_7 = os.environ["STRING_SESSION_7"]
+    STRING_SESSION_8 = os.environ["STRING_SESSION_8"]
+    STRING_SESSION_9 = os.environ["STRING_SESSION_9"]
+    STRING_SESSION_10 = os.environ["STRING_SESSION_10"]
 
 class DATABASE:
     MONGO_DB_URL = os.environ["MONGO_DB_URL"]
