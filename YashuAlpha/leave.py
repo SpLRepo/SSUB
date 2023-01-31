b6bf28fffdddeca5324fce028fd10847bd25cc25
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-31 12:07:54 +0530

    Update leave.py

--- YashuAlpha/leave.py
+++ YashuAlpha/leave.py
@@ -1,9 +1,12 @@
 from config import DEV
 from .data import KeshavX
 from .verify import verify
+from pyrogram import Client, filters
+from . import hl
 
 LEGENDS = DEV.SUDO_USERS + [DEV.OWNER_ID] + KeshavX
 
+@Client.on_message(filters.command("leave", hl))
 async def leave(_, m):
     if not await verify(m.from_user.id):
         return
