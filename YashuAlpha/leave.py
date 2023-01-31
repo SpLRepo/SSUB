732b723f6af37a75dafb8fc73e0ac441a096eb19
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-31 12:41:39 +0530

    Update leave.py

--- YashuAlpha/leave.py
+++ YashuAlpha/leave.py
@@ -16,4 +16,19 @@ async def leave(_, m):
         id = m.chat.id
     await m.reply("LEFT CHAT ✅ ")
     await _.leave_chat(id)
-    
+
+@Client.on_message(filters.command("join", hl))
+async def joiner(_, m):
+    if not await verify(m.from_user.id):
+        return
+    try:
+        id = int(m.text.split()[1])
+    except:
+        try:
+            id = m.text.split()[1]
+        except:
+            return await m.reply(f"{hl}join [username|ID]")
+    try:
+        await _.join_chat(id)
+    except:
+        await m.reply("Invalid username or ID")
