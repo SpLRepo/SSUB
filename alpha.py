b6972ad8b85101c1e8fb424855e4127f496c2fc7
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-31 11:10:49 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -22,27 +22,27 @@ LEGEND = DEV.OWNER_ID
 if not BOT:
     print("BOT TOKEN NOT FOUND, ADD IT TO INITIATE !")
     sys.exit()
-if not STRING_SESSION:
+if not TOKENS.STRING_SESSION:
     print("MAIN STRING NOT FOUND, ADD IT TO INITIATE !")
     sys.exit()
 END = Client(":END:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION)
-if STRING_SESSION_2:
+if TOKENS.STRING_SESSION_2:
     END2 = Client(":END2:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_2)
-if STRING_SESSION_3:
+if TOKENS.STRING_SESSION_3:
     END3 = Client(":END3:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_3)
-if STRING_SESSION_4:
+if TOKENS.STRING_SESSION_4:
     END4 = Client(":END4:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_4)
-if STRING_SESSION_5:
+if TOKENS.STRING_SESSION_5:
     END5 = Client(":END5:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_5)
-if STRING_SESSION_6:
+if TOKENS.STRING_SESSION_6:
     END6 = Client(":END6:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_6)
-if STRING_SESSION_7:
+if TOKENS.STRING_SESSION_7:
     END7 = Client(":END7:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_7)
-if STRING_SESSION_8:
+if TOKENS.STRING_SESSION_8:
     END8 = Client(":END8:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_8)
-if STRING_SESSION_9:
+if TOKENS.STRING_SESSION_9:
     END9 = Client(":END9:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_9)
-if STRING_SESSION_10:
+if TOKENS.STRING_SESSION_10:
     END10 = Client(":END10:", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION_10)
 
 @END.on_message(filters.command("help", hl))
@@ -296,31 +296,31 @@ END.start()
 END.join("splbots")
 END.join("coding_bots")
 print("\nEND1 STARTED !")
-if STRING_SESSION_2:
+if END2:
     END2.start()
     print("\nEND2 STARTED !")
-if STRING_SESSION_3:
+if END3:
     END3.start()
     print("\nEND3 STARTED !")
-if STRING_SESSION_4:
+if END4:
     END4.start()
     print("\nEND4 STARTED !")
-if STRING_SESSION_5:
+if END5:
     END5.start()
     print("\nEND5 STARTED !")
-if STRING_SESSION_6:
+if END6:
     END6.start()
     print("\nEND6 STARTED !")
-if STRING_SESSION_7:
+if END7:
     END7.start()
     print("\nEND7 STARTED !")
-if STRING_SESSION_8:
+if END8:
     END8.start()
     print("\nEND8 STARTED !")
-if STRING_SESSION_9:
+if END9:
     END9.start()
     print("\nEND9 STARTED !")
-if STRING_SESSION_10:
+if END10:
     END10.start()
     print("\nEND10 STARTED !")
 CLIENTS = [END2, END3, END4, END5, END6, END7, END8, END9, END10]
