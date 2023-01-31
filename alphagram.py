ce5225d69f894b2c78de2e502d049e459c5a055b
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-31 11:37:28 +0530

    Update alphagram.py

--- alphagram.py
+++ alphagram.py
@@ -2,6 +2,6 @@ from typing import Callable
 
 class Alpha:
     def on_message(command, hl):
-        async def decor(_, m):
+        async def decor(_, m, *args, **kwargs):
             return
         return decor
