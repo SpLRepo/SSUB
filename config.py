0e0daf837a15bcffa37bf0d51c7f9f90c6f66e45
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-31 11:01:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,37 +1,34 @@
 import os
 
 class API:
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    API_ID = int(os.getenv("API_ID", ""))
+    API_HASH = os.getenv("API_HASH", "")
 
 class TOKENS:
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    STRING_SESSION = os.environ["STRING_SESSION"]
-    try:
-        STRING_SESSION_2 = os.environ["STRING_SESSION_2"]
-        STRING_SESSION_3 = os.environ["STRING_SESSION_3"]
-        STRING_SESSION_4 = os.environ["STRING_SESSION_4"]
-        STRING_SESSION_5 = os.environ["STRING_SESSION_5"]
-        STRING_SESSION_6 = os.environ["STRING_SESSION_6"]
-        STRING_SESSION_7 = os.environ["STRING_SESSION_7"]
-        STRING_SESSION_8 = os.environ["STRING_SESSION_8"]
-        STRING_SESSION_9 = os.environ["STRING_SESSION_9"]
-        STRING_SESSION_10 = os.environ["STRING_SESSION_10"]
-    except:
-        pass
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    STRING_SESSION = os.getenv("STRING_SESSION", "")
+    STRING_SESSION_2 = os.getenv("STRING_SESSION_2", "")
+    STRING_SESSION_3 = os.getenv("STRING_SESSION_3", "")
+    STRING_SESSION_4 = os.getenv("STRING_SESSION_4", "")
+    STRING_SESSION_5 = os.getenv("STRING_SESSION_5", "")
+    STRING_SESSION_6 = os.getenv("STRING_SESSION_6", "")
+    STRING_SESSION_7 = os.getenv("STRING_SESSION_7", "")
+    STRING_SESSION_8 = os.getenv("STRING_SESSION_8", "")
+    STRING_SESSION_9 = os.getenv("STRING_SESSION_9", "")
+    STRING_SESSION_10 = os.getenv("STRING_SESSION_10", "")
 
 class DATABASE:
-    MONGO_DB_URL = os.environ["MONGO_DB_URL"]
+    MONGO_DB_URL = os.getenv("MONGO_DB_URL", "")
 
 class DEV:
-    OWNER_ID = int(os.environ["OWNER_ID"])
+    OWNER_ID = int(os.getenv("OWNER_ID", ""))
     
     # DONT EDIT THIS 
     SUDO_USERS = [5868832590] 
     # YOU CAN ADD SUDO USING /addsudo
 
 class STUFF:
-    ALIVE_PIC = os.environ["ALIVE_PIC"]
-    HELP_PIC = os.environ["HELP_PIC"]
-    START_PIC = os.environ["START_PIC"]
-    COMMAND_HANDLER = os.environ["COMMAND_HANDLER"]
+    ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+    HELP_PIC = os. getenv("HELP_PIC", "")
+    START_PIC = os. getenv("START_PIC", "")
+    COMMAND_HANDLER = os. getenv("COMMAND_HANDLER", "")
