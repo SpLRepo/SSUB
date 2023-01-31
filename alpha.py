1b7336f876635de406228816c9dfdf6eeda811a8
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2023-01-31 12:18:10 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters, idle
 from config import *
 import time
+"""
 from YashuAlpha.plugins import start, help, cmds_cbq, close_cbq, raid_cbq, extra_cbq, spam_cbq
 from YashuAlpha.raid import raid, replyraid, dreplyraid, raid_cwf
 from YashuAlpha.data import KeshavX
@@ -10,6 +11,7 @@ from YashuAlpha.leave import leave
 from YashuAlpha.sudo import add_or_del_sudo, sudo_users
 from YashuAlpha.alive_ping import ping, aliver 
 from YashuAlpha.inline import inl
+"""
 import sys
 from external_client import BOT
 from alphagram import Alpha
